fcca310249eb01fbe0badae53d0f120195c71c59
James Brock <jamesbrock@gmail.com>
2021-04-17 18:41:54 +0900

    WIP

--- shell.nix
+++ shell.nix
@@ -7,10 +7,10 @@ pkgs.mkShell {
     pkgs.nodejs-14_x
     # pkgs.nodePackages.bower
 
-    pkgs.nodePackages.webpack
-    pkgs.nodePackages.webpack-cli
-    pkgs.nodePackages.webpack-dev-server
-    pkgs.nodePackages.purescript-psa
+    # pkgs.nodePackages.webpack
+    # pkgs.nodePackages.webpack-cli
+    # pkgs.nodePackages.webpack-dev-server
+    # pkgs.nodePackages.purescript-psa
 
     # pkgs.nodePackages.long
     # pkgs.nodePackages.purs-loader
@@ -19,6 +19,7 @@ pkgs.mkShell {
   LC_ALL = "C.UTF-8"; # https://github.com/purescript/spago/issues/507
   # echo "npm install --save-dev purs-loader html-webpack-plugin long"
   # echo "npm install --save-dev webpack webpack-cli webpack-dev-server purescript-psa"
+  # echo "webpack-dev-server --progress --inline --hot"
   shellHook = ''
   echo ""
   echo "spago build"
@@ -28,7 +29,7 @@ pkgs.mkShell {
   echo ""
   echo "https://github.com/purescript/spago#get-started-from-scratch-with-webpack-frontend-projects"
   echo "npm install --save-dev webpack webpack-cli webpack-dev-server purescript-psa purs-loader html-webpack-plugin long"
-  echo "webpack-dev-server --progress --inline --hot"
+  echo "npm run webpack:server"
   '';
 }
 
